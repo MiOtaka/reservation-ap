ae830a4b0e3688afc7a4ca30e10d4f63f0d249df
FJN大髙 <m.otaka@fujinetworks.co.jp>
2024-07-24 15:00:49 +0900

    ログイン＆新規登録実装

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -13,7 +13,11 @@ export class AuthService {
 
     // }
 
-    // getProductById(productId: string): Observable<any> {
-    //     return this.http.get('/api/v1/products/' + productId)
-    // }
+    register(userData: any): Observable<any> {
+        return this.http.post('/api/v1/users/register', userData)
+    }
+
+    login(userData: any): Observable<any> {
+        return this.http.post('/api/v1/users/login', userData)
+    }
 }
