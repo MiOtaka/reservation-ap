3f6ff5d8e89ef111aa3e37ddb02735d3f1cefd11
FJN大髙 <m.otaka@fujinetworks.co.jp>
2024-07-25 10:42:41 +0900

    users.jsのインポート追加(エラー修正)

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../model/user')
+const config = require('../config')
 const jwt = require('jsonwebtoken')
 
 router.post('/login', async function(req, res) {
@@ -13,8 +14,8 @@ router.post('/login', async function(req, res) {
         return res.status(422).send({errors: [{title: 'User error', detail: 'Please fill passsword!'}]})
     }
     
-    foundUser = User.findOne({email})
     try {
+        const foundUser = await User.findOne({email})
         if(!foundUser){
             return res.status(422).send({errors: [{title: 'User error', detail: 'User is not exist!'}]})
           }
@@ -22,16 +23,15 @@ router.post('/login', async function(req, res) {
             return res.status(422).send({errors: [{title: 'User error', detail: 'Incorrect password!'}]})
         }
         const token = jwt.sign({
-            userId: foundUser.Id,
+            userId: foundUser.id,
             username: foundUser.username
         }, config.SECRET, { expiresIn: '1h'})
 
-
         return res.json(token)
-    } catch(err) {
-        return res.status(422).send({errors: [{title: 'User error', detail: 'Something went wrong!'}]})
-    }
-})
+        } catch(err) {
+            return res.status(422).send({errors: [{title: 'User error', detail: 'Something went wrong!'}]})
+        }
+    })
 
 router.post('/register', async function(req, res) {
     const { username, email, password, confirmPassword } = req.body
@@ -49,8 +49,8 @@ router.post('/register', async function(req, res) {
         return res.status(422).send({errors: [{title: 'User error', detail: 'Please check passwords!'}]})
     }   
 
-    foundUser = await User.findOne({email})
     try {
+        const foundUser = await User.findOne({email})
         if(foundUser){
             return res.status(422).send({errors: [{title: 'User error', detail: 'User already exist!'}]})
           }
