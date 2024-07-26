645da8aaeba2301579c6ac9650e90c81616a5e20
FJN大髙 <m.otaka@fujinetworks.co.jp>
2024-07-26 09:39:36 +0900

    AuthMiddleとAuthGuard追加

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -8,8 +8,6 @@ function notAuthorized(res) {
     return res.status(401).send({errors: [{title: 'Not Authorized', detail: 'You need to login!'}]})
 }
 
-
-
 exports.authMiddleware = function(req, res, next) {
     const token = req.headers.authorization
 
@@ -17,20 +15,31 @@ exports.authMiddleware = function(req, res, next) {
         return notAuthorized(res)
     }
 
-    jwt.verify(token.split(' ')[1], config.SECRET, function(err, decodedToken) {
+    jwt.verify(token.split(' ')[1], config.SECRET, async function(err, decodedToken) {
         if(err) {
             return res.status(401).send({errors: [{title: 'Not Authorized', detail: 'Invalid Token!'}]})
         }
         
-        User.findById(decodedToken.userId, function(err, foundUser){
-            if(err) {
-                return res.status(401).send({errors: [{title: 'Not Authorized', detail: 'Invalid Token!'}]})
-            }
+        // User.findById(decodedToken.userId, async function(err, foundUser){
+        //     if(err) {
+        //         return res.status(401).send({errors: [{title: 'Not Authorized', detail: 'Invalid Token!'}]})
+        //     }
+        //     if(!foundUser) {
+        //         return res.status(401).send({errors: [{title: 'Not Authorized', detail: 'User Not found!'}]})
+        //     }
+        //     next()
+        // })
+
+        try {
+            const foundUser = await User.findById(decodedToken.userId)
             if(!foundUser) {
                 return res.status(401).send({errors: [{title: 'Not Authorized', detail: 'User Not found!'}]})
             }
-            next()
-        })
+             next()
+          } catch (err) {
+            return res.status(401).send({errors: [{title: 'Not Authorized', detail: 'Invalid Token!'}]})
+          }
+       
     })
 
     next()
